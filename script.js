c354e5e322c6610594445f09301c7155595ed447
Yehiashamel <102812060+Yehiashamel@users.noreply.github.com>
2022-07-12 05:06:32 +0200

    s

---
+++ script.js
@@ -0,0 +1,29 @@
+let saturate = document.querySelector('#saturate');
+let contrast = document.getElementById('contrast');
+let brightness = document.getElementById('brightness');
+let sepia = document.getElementById('sepia');
+let grayscale = document.getElementById('grayscale');
+let blur = document.getElementById('blur');
+let hueRotate = document.getElementById('hue-rotate');
+
+let download = document.getElementById('download');
+let reset = document.getElementById('Reset');
+
+let upload = document.getElementById('upload');
+let imgBox = document.querySelector('.img-box');
+
+window.onload = function(){
+    download.style.display = 'none';
+    reset.style.display = 'none';
+    imgBox.style.display = 'none';
+}
+upload.onchange = function(){
+    download.style.display = 'block';
+    reset.style.display = 'block';
+    imgBox.style.display = 'block';
+    let file = new FileReader();
+    file.readAsDataURL(upload.files[0]);
+    file.onload = function(){
+    img.src = file.result;
+    }
+}
